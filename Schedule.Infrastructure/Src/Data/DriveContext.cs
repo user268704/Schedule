058546e3cc11e1adbcb1a4e5be1ad5ef9398cb95
user268704 <vanagagarin7@gmail.com>
2022-06-29 00:53:54 +0300

    Всё

--- Schedule.Infrastructure/Src/Data/DriveContext.cs
+++ Schedule.Infrastructure/Src/Data/DriveContext.cs
@@ -19,11 +19,32 @@ public class DriveContext : IRouteData
 
     private async Task<IEnumerable<RouteItem>?> GetRoutesAsync()
     {
-        var httpResponse =  _http.GetAsync(_configuration.GetConnectionString("Drive")).Result;
-
-        return Deserialize(await httpResponse.Content.ReadAsStringAsync());
+        string url = _configuration["DriveToken"];
+        string downloadUrl = GetDownloadUrl(url);
+        
+        var response = _http.GetAsync(downloadUrl).Result; 
+        var content = response.Content.ReadAsStringAsync();
+        var routes = JsonConvert.DeserializeObject<IEnumerable<RouteItem>>(content.Result);
+        return routes;
     }
-    
+
+    private string GetDownloadUrl(string token)
+    {
+        string url = "https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key=" + "https://disk.yandex.ru/d/mpdfcmS4SC8RgA";
+        using (HttpRequestMessage message = new(HttpMethod.Get, url)) 
+        {
+            message.Headers.Add("Authorization", "OAuth " + token);
+            message.Headers.Add("Host", "cloud-api.yandex.net");
+            var response = _http.SendAsync(message).Result;
+
+            // get href from json response
+            var json = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result);
+            var href = json.RootElement.GetProperty("href").GetString();
+            
+            return href;
+        }
+    }
+
     private IEnumerable<RouteItem>? Deserialize(string json)
     {
         return JsonConvert.DeserializeObject<IEnumerable<RouteItem>>(json);
